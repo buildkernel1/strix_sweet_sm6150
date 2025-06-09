007e4bf078e94faa615d3f3e7d3a4f659a19b12f
miyu-xzy <miyu775566@gmail.com>
2025-06-09 16:48:39 +0700

    fingerprint: fpc: cleanup! & optimized
    Signed-off-by: miyu-xzy <miyu775566@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -33,11 +33,14 @@
 #include <linux/atomic.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
+#include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #ifndef FPC_DRM_INTERFACE_WA
-#include <linux/msm_drm_notify.h>
+
+#include <drm/drm_notifier.h>
+
 #endif
 #include <linux/mutex.h>
 #include <linux/of.h>
@@ -48,6 +51,10 @@
 #include <linux/fb.h>
 #include <linux/pinctrl/qcom-pinctrl.h>
 #include <drm/drm_bridge.h>
+
+
+#define FPC1020_NAME "fpc1020"
+
 
 #define FPC_GPIO_NO_DEFAULT -1
 #define FPC_GPIO_NO_DEFINED -2
@@ -92,7 +99,13 @@ static struct vreg_config vreg_conf[] = {
 	/*{ "vdd_io", 1800000UL, 1800000UL, 6000, }, */
 };
 
+
+#ifdef CONFIG_MACH_XIAOMI_SWEET
+static int power_cfg = 1;
+#else
 static int power_cfg = 0;
+#endif
+
 
 struct fpc1020_data {
 	struct device *dev;
@@ -120,13 +133,18 @@ struct fpc1020_data {
 	atomic_t wakeup_enabled;	/* Used both in ISR and non-ISR */
 	int irqf;
 #ifndef FPC_DRM_INTERFACE_WA
-	struct notifier_block notifier;
+
+	struct notifier_block fb_notifier;
+
 #endif
 	bool fb_black;
 	bool wait_finger_down;
 #ifndef FPC_DRM_INTERFACE_WA
 	struct work_struct work;
 #endif
+
+struct input_handler input_handler;
+
 };
 
 static int reset_gpio_res(struct fpc1020_data *fpc1020);
@@ -871,6 +889,63 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+static int input_connect(struct input_handler *handler,
+		struct input_dev *dev, const struct input_device_id *id) {
+	int rc;
+	struct input_handle *handle;
+	struct fpc1020_data *fpc1020 =
+		container_of(handler, struct fpc1020_data, input_handler);
+
+	if (!strstr(dev->name, "uinput-fpc"))
+		return -ENODEV;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = FPC1020_NAME;
+	handle->private = fpc1020;
+
+	rc = input_register_handle(handle);
+	if (rc)
+		goto err_input_register_handle;
+
+	rc = input_open_device(handle);
+	if (rc)
+		goto err_input_open_device;
+
+	return 0;
+
+err_input_open_device:
+	input_unregister_handle(handle);
+err_input_register_handle:
+	kfree(handle);
+	return rc;
+}
+
+static bool input_filter(struct input_handle *handle, unsigned int type,
+		unsigned int code, int value)
+{
+	return true;
+}
+
+static void input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) },
+	},
+	{ },
+};
+
 #ifndef FPC_DRM_INTERFACE_WA
 static void notification_work(struct work_struct *work)
 {
@@ -937,8 +1012,11 @@ static int fpc_fb_notif_callback(struct notifier_block *nb,
 				 unsigned long val, void *data)
 {
 	struct fpc1020_data *fpc1020 = container_of(nb, struct fpc1020_data,
-						    notifier);
-	struct msm_drm_notifier *evdata = data;
+
+
+	fb_notifier);
+	struct fb_event *evdata = data;					    
+
 	unsigned int blank;
 
 	if (!fpc1020)
@@ -1043,6 +1121,19 @@ static int fpc1020_probe(struct platform_device *pdev)
 	if (!fpc1020->ttw_wl)
 		return -ENOMEM;
 
+    
+    fpc1020->input_handler.filter = input_filter;
+	fpc1020->input_handler.connect = input_connect;
+	fpc1020->input_handler.disconnect = input_disconnect;
+	fpc1020->input_handler.name = FPC1020_NAME;
+	fpc1020->input_handler.id_table = ids;
+	rc = input_register_handler(&fpc1020->input_handler);
+	if (rc) {
+		dev_err(dev, "failed to register key handler\n");
+		goto exit;
+	}
+    
+
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
 		dev_err(dev, "fpc could not create sysfs\n");
@@ -1066,7 +1157,9 @@ static int fpc1020_probe(struct platform_device *pdev)
 #ifndef FPC_DRM_INTERFACE_WA
 	INIT_WORK(&fpc1020->work, notification_work);
 	fpc1020->notifier = fpc_notif_block;
-	msm_drm_register_client(&fpc1020->notifier);
+
+	drm_register_client(&fpc1020->notifier);
+
 #endif
 
 	//rc = hw_reset(fpc1020);
@@ -1083,7 +1176,9 @@ static int fpc1020_remove(struct platform_device *pdev)
 	struct fpc1020_data *fpc1020 = platform_get_drvdata(pdev);
 
 #ifndef FPC_DRM_INTERFACE_WA
-	msm_drm_unregister_client(&fpc1020->notifier);
+
+	drm_unregister_client(&fpc1020->fb_notifier);
+
 #endif
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
@@ -1108,7 +1203,9 @@ MODULE_DEVICE_TABLE(of, fpc1020_of_match);
 
 static struct platform_driver fpc1020_driver = {
 	.driver = {
-		   .name = "fpc1020",
+
+		   .name = "FPC1020_NAME",
+
 		   .owner = THIS_MODULE,
 		   .of_match_table = fpc1020_of_match,
 		   },
